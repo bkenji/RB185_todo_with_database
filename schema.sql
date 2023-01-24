511709a652c9da844455739dcc1e7f508e2dea4a
Brenno Kenji <brennokenji@gmail.com>
2023-01-23 20:46:18 -0800

    Multi-user functionality + hashed passwords - final

--- schema.sql
+++ schema.sql
@@ -2,7 +2,9 @@ DROP TABLE IF EXISTS lists;
 
 CREATE TABLE lists (
   id serial PRIMARY KEY,
-  name text UNIQUE NOT NULL 
+  name text NOT NULL,
+  user_id int NOT NULL REFERENCES users(id),
+  UNIQUE (name, user_id)
 );
 
 DROP TABLE IF EXISTS todos;
@@ -13,4 +15,13 @@ CREATE TABLE todos (
   completed boolean NOT NULL DEFAULT false,
   list_id int NOT NULL REFERENCES lists(id)
    ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id serial PRIMARY KEY,
+  username text NOT NULL UNIQUE,
+  hashed_pw text NOT NULL
+  CHECK (username ~ '^[A-Za-z0-9_]+$')
 );
