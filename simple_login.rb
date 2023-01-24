7a02d9a5a8aa14c98c3a621fb1e8f4cc7774789f
Brenno Kenji <brennokenji@gmail.com>
2023-01-23 20:20:04 -0800

    Multi-user functionality + hashed passwords - final

--- simple_login.rb
+++ simple_login.rb
@@ -1,13 +1,6 @@
 # Set of helper methods to facilitate database-based sign up and user authentication. To be required by the file containing database logic (e.g. databasepersistence.rb)
 
 
-require "pg" # necessary?
-
-
-# Methods to be used as `helpers` for the Sinatra app (i.e. they could be inside the `helpers block`)
-
-
-
 def valid_login?(username, password)
   existing_username?(username) && 
   valid_password?(username, password)
